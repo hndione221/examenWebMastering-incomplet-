8e45ce2a6cf2543f484c9090e9d5063ef4ba3227
hndione221 <77298232+hndione221@users.noreply.github.com>
2021-06-30 22:47:08 +0000

    Add files via upload

---
+++ projet webmastering/projet webmastering/config/constante.php
@@ -0,0 +1,7 @@
+<?php
+//représente toutes les constantes
+ //chemin absolu pour l'inclusion de nos fichiers
+ define("PATH_ROOT",str_replace("Public","",$_SERVER["DOCUMENT_ROOT"]));
+ //chemin des actions des formulaires et des liens
+ define("WEB_ROOT","http://localhost:9000/index.php");
+?>
---
+++ projet webmastering/projet webmastering/config/require.php
@@ -0,0 +1,7 @@
+<?php
+//require charge tous les fichers se trouvant dans core et models
+    require_once(PATH_ROOT."core/session.php");
+    require_once(PATH_ROOT."core/helper.php");
+    
+
+?>
